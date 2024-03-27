f76e49eaafdad20facc3531ae80977cacde35afc
1Bc <mycodeisx@gmail.com>
2024-03-27 21:45:42 +0100

    example API fetch

--- src/App.vue
+++ src/App.vue
@@ -44,7 +44,19 @@ export default {
   methods: {
     addPerson(person) {
       this.persons = [...this.persons, person]
+    },
+    async getPersons() {
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        const data = await response.json()
+        this.persons = data
+      } catch (error) {
+        console.error(error)
+      }
     }
+  },
+  mounted() {
+    this.getPersons()
   }
 }
 </script>
