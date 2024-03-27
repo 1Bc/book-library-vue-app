26f8cd0ee951b97bf6814870cbbdc35d9907f315
1Bc <mycodeisx@gmail.com>
2024-03-27 21:20:19 +0100

    create table

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,57 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="small-container">
+    <h1>Znajomi</h1>
+
+    <persons-table :personsSource="persons"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PersonsTable from './components/PersonsTable.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PersonsTable
+  },
+  data() {
+    return {
+      persons: [
+        {
+          id: 1,
+          name: 'Adam Słodowy',
+          email: 'adam.slodowy@zrobtosam.pl',
+          phone: '+48 787 774 664'
+        },
+        {
+          id: 2,
+          name: 'Michał Studencki',
+          email: 'ms@student.pwr.edu.pl',
+          phone: '+48 600 565 454'
+        },
+        {
+          id: 3,
+          name: 'Kamila Napokaz',
+          email: 'kami2003@h2.pl',
+          phone: '+48 609 554 987'
+        }
+      ]
+    }
   }
 }
 </script>
-
+export default {
+  name: 'persons-table',
+  props: {
+    personsSource: Array
+  }
+}
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+button {
+  background: #009435;
+  border: 1px solid #009435;
+}
+.small-container {
+  max-width: 680px;
 }
 </style>
